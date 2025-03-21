48388206aa5a4ff87d0c0478a8226b7b5cd2d806
Andrea Camilla Puglia <apuglia@lxplus976.cern.ch>
2025-03-21 17:55:21 +0100

    Aggiunti files per le PFCands

--- python/postprocessing/ML/Scores_graph.py
+++ python/postprocessing/ML/Scores_graph.py
@@ -94,7 +94,7 @@ def working_points_values(histo, nbins, wp):
             out_i = i
     return out_rej, out_i
         
-def working_points(histo1,histo2 = None,histo3 = None,wp):
+def working_points(histo1,histo2 = None,histo3 = None,wp = None):
     nbins = histo1.GetNbinsX()
     found = False 
     for i in range(1,nbins):
@@ -105,17 +105,17 @@ def working_points(histo1,histo2 = None,histo3 = None,wp):
             print('Working point: ', wp)
             print('score cut: ', score_cut)
             print(histo1.GetName(), ' value: ', rej)
-            if histo2 not None:
+            if histo2 != None:
                 histo2_value = histo2.Integral(i,nbins)
                 print(histo2.GetName(), ' value: ', histo2_value)
-            if histo3 not None:
+            if histo3 != None:
                 histo3_value = histo3.Integral(i,nbins)
                 print(histo3.GetName(), ' value: ', histo3_value)
-    if histo2 not None and histo3 not None:
+    if histo2 != None and histo3 != None:
         return score_cut, rej, histo2_value, histo3_value
-    elif histo2 not None:
+    elif histo2 != None:
         return score_cut, rej, histo2_value
-    elif histo3 not None:
+    elif histo3 != None:
         return score_cut, rej, histo3_value
     else:
         return score_cut, rej
--- python/postprocessing/tools.py
+++ python/postprocessing/tools.py
@@ -268,9 +268,22 @@ def top3j1fj(fj, j0, j1, j2, dr0=None, dr1=None, dr2=None):
 
 
 def get_jet(jets):
-    return list(filter(lambda x : x.jetId and x.pt>25 , jets))
+    goodjet = list(filter(lambda x : x.jetId and x.pt>25 , jets))
+    idx_good = 0
+    for idx_jet in range(len(jets)):
+        if jets[idx_jet] in goodjet:
+            goodjet[idx_good].jetIdx = idx_jet
+            idx_good += 1
+    return goodjet
 def get_fatjet(fatjets):
-    return list(filter(lambda x : x.jetId , fatjets))###
+    goodfatjets = list(filter(lambda x : x.jetId , fatjets))
+    idx_good = 0
+    for idx_fj in range(len(fatjets)):
+        if fatjets[idx_fj] in goodfatjets:
+            goodfatjets[idx_good].fatjetIdx = idx_fj
+            idx_good += 1
+    return goodfatjets
+
 def presel(jets, fatjets): #returns 2 collections of jets and fatjets
     goodjets = get_jet(jets)
     goodfatjets = get_fatjet(fatjets)
